83e2583467f5047c3effd4797e1edc9ab6f75108
sherry <weixiaoli0210@163.com>
2018-02-01 17:58:27 +0800

    小程序初始化

---
+++ app.js
@@ -0,0 +1,10 @@
+//app.js 整个小程序只有一个App实例，是全部页面共享的
+App({
+  //小程序启动之后触发
+  onLaunch: function () {
+    
+  },
+  globalData: {
+    
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,66 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  //参与页面渲染的数据
+  data: {
+    swiper:{
+      autoplay:true,
+      'indicator-color': '#898989',
+      'indicator-active-color': '#be0000',
+      'indicator-dots':true,
+      interval:2000,
+      imgUrls: [
+        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ]
+    },
+    advertise:{
+      imgUrls: [
+        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ]
+    },
+    hotProducts:[
+      {
+        imgUrl:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        name:'Huile Olive 食用橄榄油',
+        price:'23.00',
+        integral:'900',
+        classname:'goods_even'
+      },
+      {
+        imgUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        name: 'Huile Olive 食用橄榄油',
+        price: '23.00',
+        integral: '900',
+        classname: 'goods_odd'
+      },
+      {
+        imgUrl: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        name: 'Huile Olive 食用橄榄油ssss',
+        price: '23.00',
+        integral: '900',
+        classname: 'goods_even'
+      },
+      {
+        imgUrl: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        name: 'Huile Olive 食用橄榄油',
+        price: '23.00',
+        integral: '900',
+        classname: 'goods_odd'
+      }
+    ],
+    banner:{
+      src:'/pages/product-detail/product-detail',
+      imgUrl:'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg'
+    }
+  },
+  //页面渲染后执行
+  onLoad:function(){
+    wx.showTabBar()
+  }
+})
