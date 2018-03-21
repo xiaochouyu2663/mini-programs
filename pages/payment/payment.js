7629442d62b80ae6a14fcc7d5f13b8392f4f0c6b
sherry <weixiaoli0210@163.com>
2018-03-22 06:32:24 +0800

    上传图片功能

--- pages/payment/payment.js
+++ pages/payment/payment.js
@@ -1,69 +1,32 @@
-// pages/payment/payment.js
+var app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
   
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  },
   radioChange: function (e){
-    console.log('radio发生change事件，携带value值为：', e.detail.value)
-  }
+    // console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+  upload: function (e) {
+    wx.chooseImage({
+      count: 1, 
+      sizeType: ['original', 'compressed'], 
+      sourceType: ['album', 'camera'], 
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths
+        wx.uploadFile({
+          url: app.globalData.URL +'/account/upload', 
+          filePath: tempFilePaths[0],
+          name: 'image',
+          header: {
+            'content-type': 'multipart/form-data'
+          },
+          formData: {
+            'userId': '123'
+          },
+          success: function (res) {
+            console.log('上传图片后返回的res', res)
+          }
+        })
+      }
+    })
+  },
 })
